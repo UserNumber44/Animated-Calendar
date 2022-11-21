1ddde0fae18745a0054904f441f295c706c327f9
Josh Nacionales <105354010@edu.cbe.ab.ca>
2022-11-21 11:58:47 -0700

    rip stickfigure and p5

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,27 @@
 <script>
-  //Calendar?
-  //Animation of stick figure per reload to signify day
-  //Uses graphic of calendar (either img or canvas)
-  //Use date function?
+  import P5 from 'p5-svelte';
+  import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 
-  //Plan
-  //Create Calendar
-  //Create stickfigure
-  //Create move cycle
-  //Create "openDoor" 
-  //Create end points per date
-  //Create "doorpoints" (probably boolean)
+  let width = 1000;
+  let height = 1000;
   
-
-
-import P5 from 'p5-svelte';
-
-let width = 1000;
-let height = 1000;
-
-let sketch = (p5) => {
-  p5.setup = () => {
-    p5.createCanvas(1000, 725);
+  //Calendar
+  let sketch = (p5) => {
+    p5.setup = () => {
+      p5.createCanvas(1000, 725);
+    };
+    p5.draw = () => {
+      p5.noStroke();
+      p5.background(100);
+      for (let i = 0; i < height; i += 143) {
+        p5.rect(0, i, width, 10);
+        p5.fill(500);
+        p5.rect(i, 0, 10, height);
+      };
+    };
   };
-  p5.draw = () => {
-  p5.background(100);
-  p5.noStroke();
-  for (let i = 0; i < height; i += 143) {
-    p5.rect(0, i, width, 10);
-    p5.fill(500);
-    p5.rect(i, 0, 10, height);
-  }
-  };
-};
-
-function stickFigure() {
-//creates stickfigure  
-}
-
-function calander() {
-//creates calander
-}
-
-function move(date) {
-//indicates date for stickfigure to move towards
-}
-
-function openDoor() {
-//allows stickfigure to move from each week/month
-}
-
-
+  //Progress bar
 
 </script>
 
